9ebec8c3fca0fb732efde0a9d3c6e416bb3592e0
RealET <real3t@gmail.com>
2021-06-10 19:13:30 +0200

    Tous les layouts sont en 2 colonnes pour #navigation et #extra sur tablettes

--- layoutgala2021.php
+++ layoutgala2021.php
@@ -71,20 +71,37 @@ div#footer p{margin:0;padding:5px 10px}
 /* un switch avec 2 types de cas  pour ipad*/
 <?php 
 switch ($layout){
-case 1:
-case 3:
-case 5:
-case 7:
-case 9:
-case 11:
-case 13:
-case 15:
-case 18:
-case 19:
-case 20:
-case 21:
-case 22:
-	?>
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 9:
+	case 11:
+	case 13:
+	case 15:
+	case 18:
+	case 19:
+	case 20:
+	case 21:
+	case 22:
+	case 23:
+	case 24:
+	case 25:
+	case 26:
+	case 27:
+	case 29:
+	case 30:
+	case 31:
+	case 32:
+	case 33:
+	case 34:
+	case 35:
+	case 36:
+	case 37:
+	case 38:
+	case 38:
+	case 39:
+?>
 	div#container {
 	display: grid;
 	grid-template-columns: 50% 50%;
@@ -110,18 +127,20 @@ case 22:
 	grid-column-start: 2;
 	grid-row-start: 3;
 	}
-	<?php
+<?php
 	break;
-case 2:
-case 4:
-case 6:
-case 8:
-case 10:
-case 12:
-case 14:
-case 16:
-case 17:
-	?>
+	case 2:
+	case 4:
+	case 6:
+	case 8:
+	case 10:
+	case 12:
+	case 14:
+	case 16:
+	case 17:
+	case 28:
+	case 40:
+?>
 	div#container {
 	display: grid;
 	grid-template-columns: 50% 50%;
@@ -147,10 +166,10 @@ case 17:
 	grid-column-start: 1;
 	grid-row-start: 3;
 	}
-	<?php
+<?php
 	break;
 	}
-	?>
+?>
 }
 
 
